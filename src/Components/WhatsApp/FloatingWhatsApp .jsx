54f8eb2de6ffa5f0369b078bac7cedd9f7259beb
Srishti <srishtigupta190202@gmail.com>
2025-06-14 18:41:12 +0530

    added whatsapp no

--- src/Components/WhatsApp/FloatingWhatsApp .jsx
+++ src/Components/WhatsApp/FloatingWhatsApp .jsx
@@ -2,7 +2,7 @@ import React from "react";
 import whatsappIcon from "../../assets/whatsapp.png";
 import "./FloatingWhatsApp.css";
 
-const phone = "9380312328"; // Replace with your WhatsApp number (country code + number, no +)
+const phone = "99804 63512"; 
 const message = "Hello! I'm interested in your products."; // Your template message
 
 const FloatingWhatsApp = () => {
